3f2a9ab5d48bd98a09e5279cba7b0c47e3c9180c
GutierrezJuaquinFranco <juaquin.gutierrez02@gmail.com>
2022-03-14 10:48:44 +0800

    My Personal Info added

---
+++ git-python-gutierrez.py
@@ -0,0 +1,9 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Juaquin Franco D. Gutierrez")
+Juaquin Franco D. Gutierrez
+>>> print("Santol, Boac, Marinduque")
+Santol, Boac, Marinduque
+>>> print("December 2, 2001")
+December 2, 2001
+>>> 
